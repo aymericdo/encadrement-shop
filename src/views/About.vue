52ecfd32b79094a62966d1a959e27a43cd53993e
aymeric dominique <aymeric.dominique@gmail.com>
2021-11-26 12:14:45 +0100

    fix position about

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,17 +2,19 @@
   <div class="about">
     <Menu></Menu>
     <div class="content">
-      <h1>À propos</h1>
-      <p>
-        On s'est dit qu'on n'allait pas s'arrêter en si bon chemin, alors pour
-        vous (et vos yeux ébahis), une page ENTIÈRE d'annonces complètement
-        vérifiées par nos soins ! :D
-      </p>
-      <p>
-        Comme ça, plus besoin de tergiverser (ni de justifier), ces annonces
-        sont de fait, légales.
-      </p>
-      <span class="sign">Aymeric et Thomas</span>
+      <div class="section">
+        <h1>À propos</h1>
+        <p>
+          On s'est dit qu'on n'allait pas s'arrêter en si bon chemin, alors pour
+          vous (et vos yeux ébahis), une page ENTIÈRE d'annonces complètement
+          vérifiées par nos soins ! :D
+        </p>
+        <p>
+          Comme ça, plus besoin de tergiverser (ni de justifier), ces annonces
+          sont de fait, légales.
+        </p>
+        <span class="sign">Aymeric et Thomas</span>
+      </div>
     </div>
   </div>
 </template>
@@ -35,7 +37,12 @@ export default defineComponent({
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  padding: 0 10em;
+  width: 100%;
+  margin-top: 5em;
+
+  .section {
+    width: 60%;
+  }
 
   & > p {
     display: flex;
@@ -50,8 +57,8 @@ export default defineComponent({
 }
 
 @media screen and (max-width: $mobileSize) {
-  .content {
-    padding: 0 0.625em;
+  .content .section {
+    width: 90%;
   }
 }
 </style>
